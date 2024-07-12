63475dc04c40f62a7fb1f9ce9c75a9e0b1608fd0
Narayan Aryal <aryal.coding@gmail.com>
2024-07-13 00:31:15 +1000

    stripe payment and auth added

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -1,59 +1,63 @@
 import express from "express";
-import { createUser, getUser } from "../model/user/UserModel.js";
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../configDb/envConfig.js";
+import { createUser, getUser, updateUser } from "../model/user/UserModel.js";
 import bcrypt from "bcryptjs";
+import {
+  signAccessJWT,
+  signRefreshJWT,
+  verifyRefreshJWT,
+} from "../utils/jwt.js";
+import { newAccessJwtAuth, userAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
-  try {
-    console.log(req.body);
-    const result = await createUser(req.body);
+// router.post("/", async (req, res, next) => {
+//   try {
+//     console.log(req.body);
+//     const result = await createUser(req.body);
 
-    console.log(result);
+//     console.log(result);
 
-    result?._id
-      ? res.json({
-          status: "success",
-          message: "User Created Successfully",
-        })
-      : res.json({
-          status: "error",
-          message: "Unable to create user, Please try again later",
-        });
-  } catch (error) {
-    if (error.message.includes("E11000 duplicate key error collection")) {
-      error.errorCode = 200;
-      error.message =
-        "There is already an account exist with this email, Please try different email";
-    }
-    next(error);
-  }
-});
+//     result?._id
+//       ? res.json({
+//           status: "success",
+//           message: "User Created Successfully",
+//         })
+//       : res.json({
+//           status: "error",
+//           message: "Unable to create user, Please try again later",
+//         });
+//   } catch (error) {
+//     if (error.message.includes("E11000 duplicate key error collection")) {
+//       error.errorCode = 200;
+//       error.message =
+//         "There is already an account exist with this email, Please try different email";
+//     }
+//     next(error);
+//   }
+// });
 
-router.post("/login", async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+// router.post("/login", async (req, res, next) => {
+//   try {
+//     const { email, password } = req.body;
 
-    // check if email exist
+//     // check if email exist
 
-    const result = await getUser({ email });
+//     const result = await getUser({ email });
 
-    console.log(result);
-    result?._id
-      ? res.json({
-          status: "success",
-          message: "User login successful",
-        })
-      : res.json({
-          status: "error",
-          message: "Unable to login user, Please try again later",
-        });
-  } catch (error) {
-    next(error);
-  }
-});
+//     console.log(result);
+//     result?._id
+//       ? res.json({
+//           status: "success",
+//           message: "User login successful",
+//         })
+//       : res.json({
+//           status: "error",
+//           message: "Unable to login user, Please try again later",
+//         });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 //google
 
@@ -63,36 +67,40 @@ router.post("/google", async (req, res, next) => {
     const { email } = req.body;
     const user = await getUser({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, JWT_SECRET);
+      const tokens = {
+        accessJWT: await signAccessJWT({ email }),
+        refreshJWT: await signRefreshJWT({ email }),
+      };
 
-      const { password, ...rest } = user._doc;
+      // const { password, ...rest } = user._doc;
       res.json({
-        rest,
-        token,
         status: "success",
         message: "Google Login Successful",
+        tokens,
       });
     } else {
       const generatePassword = Math.random().toString(36).slice(-8);
       const hashedPassword = bcrypt.hashSync(generatePassword, 10);
       const newUser = await createUser({
-        username:
-          req.body.name.split(" ").join("").toLowerCase() +
-          Math.random().toString(36).slice(-4),
+        // username:
+        //   req.body.name.split(" ").join("").toLowerCase() +
+        //   Math.random().toString(36).slice(-4),
+        name: req.body.name,
         email: req.body.email,
         password: hashedPassword,
         avatar: req.body.photo,
       });
-      // console.log("newUser", newUser);
       if (newUser._id) {
-        const token = jwt.sign({ id: newUser._id }, JWT_SECRET);
+        const tokens = {
+          accessJWT: await signAccessJWT({ email }),
+          refreshJWT: await signRefreshJWT({ email }),
+        };
 
-        const { password, ...rest } = newUser._doc;
+        // const { password, ...rest } = user._doc;
         res.json({
           status: "success",
-          message: "Google login successful",
-          token,
-          rest,
+          message: "Google Login Successful",
+          tokens,
         });
       }
     }
@@ -107,32 +115,58 @@ router.post("/google", async (req, res, next) => {
 
 //get user
 
-// router.get("/", adminAuth, (req, res, next) => {
-// router.get("/", (req, res, next) => {
-//   try {
-//     res.json({
-//       status: "success",
-//       user: req.userInfo,
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+router.get("/", userAuth, (req, res, next) => {
+  try {
+    res.json({
+      status: "success",
+      user: req.userInfo,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+router.put("/", userAuth, async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const result = await updateUser(req.body);
+    if (result?._id) {
+      const tokens = {
+        accessJWT: await signAccessJWT({ email }),
+        refreshJWT: await signRefreshJWT({ email }),
+      };
+      res.json({
+        status: "success",
+        message: "User updated successfully",
+        tokens,
+      });
+    } else {
+      res.json({
+        status: "error",
+        message: "Unable to update user please try again",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//get new access token
+router.get("/new-accessjwt", newAccessJwtAuth);
 
 // logout user
-// router.patch("/logout", async (req, res, next) => {
-//   try {
-//     const { authorization } = req.headers;
-//     const { email } = verifyRefreshJWT(authorization);
-//     email && (await findUserAndUpdate({ email }, { refreshJWT: "" }));
+router.patch("/logout", async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const { email } = verifyRefreshJWT(authorization);
+    email && (await updateUser({ email }, { refreshJWT: "" }));
 
-//     res.json({
-//       status: "success",
-//       message: "logout success",
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+    res.json({
+      status: "success",
+      message: "logout success",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,20 +1,43 @@
 import Jwt from "jsonwebtoken";
-import { ACCESS_JWT, REFRESH_JWT } from "../configDb/envConfig.js";
+import { createSession } from "../model/session/SessionModel.js";
 import { updateUser } from "../model/user/UserModel.js";
+import { ACCESS_JWT, REFRESH_JWT } from "../configDb/envConfig.js";
 
+//payload must be an object
 export const signAccessJWT = async (payload) => {
   const token = Jwt.sign(payload, ACCESS_JWT, { expiresIn: "60m" });
+
+  //store in session table
+
+  await createSession({ token });
 
   return token;
 };
 
 export const signRefreshJWT = async (payload) => {
   const token = Jwt.sign(payload, REFRESH_JWT, {
-    expiresIn: "30d",
+    expiresIn: "10d",
   });
 
   // store thr user in user table
 
   await updateUser(payload, { refreshJWT: token });
   return token;
+};
+
+//verify token
+
+export const verifyAccessJWT = (token) => {
+  try {
+    return Jwt.verify(token, ACCESS_JWT);
+  } catch (error) {
+    return error.message;
+  }
+};
+export const verifyRefreshJWT = (token) => {
+  try {
+    return Jwt.verify(token, ACCESS_JWT);
+  } catch (error) {
+    return error.message;
+  }
 };
